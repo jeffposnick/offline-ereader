d8451db702a348507b5d5dad2f92f9960abb2f56
Jeff Posnick <jeffy@google.com>
2014-10-26 17:06:18 -0400

    Chapters and scrolling.

--- js/read.js
+++ js/read.js
@@ -4,4 +4,21 @@
   var template = document.querySelector('#page-template');
   template.fontFamily = 'sans-serif';
   template.bookUrl = window.location.search.split('=')[1];
+
+  template.addEventListener('template-bound', function() {
+    var pageableText = document.querySelector('pageable-text');
+
+    template.handleKeyPress = function(e) {
+      switch (e.detail.key) {
+        case 'right':
+        case 'space':
+          pageableText.nextPage();
+        break;
+
+        case 'left':
+          pageableText.previousPage();
+        break;
+      }
+    };
+  });
 })();
