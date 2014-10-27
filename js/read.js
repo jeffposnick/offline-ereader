115a7992515f88c55cb2c93b6b63850f9d426175
Jeff Posnick <jeffy@google.com>
2014-10-26 21:56:39 -0400

    Lots more of the UI.

--- js/read.js
+++ js/read.js
@@ -4,9 +4,27 @@
   var template = document.querySelector('#page-template');
   template.fontFamily = 'sans-serif';
   template.bookUrl = window.location.search.split('=')[1];
+  template.showToolbars = true;
 
   template.addEventListener('template-bound', function() {
-    var pageableText = document.querySelector('pageable-text');
+    var pageableText = document.querySelector('#text');
+    var chaptersMenu = document.querySelector('#chapters-menu');
+    template.jumpToChapter = function(e) {
+      pageableText.scrollToElement(e.target.templateInstance.model.chapterElement);
+      chaptersMenu.toggle();
+      template.toggleToolbars();
+    };
+
+    var toolbarAnimation = document.querySelector('#toolbar-animation');
+    toolbarAnimation.target = [].slice.call(document.querySelectorAll('core-toolbar'));
+    template.toggleToolbars = function() {
+      toolbarAnimation.direction = template.showToolbars ? 'forward' : 'reverse';
+      template.showToolbars = true;
+      toolbarAnimation.play();
+    };
+    template.finishToolbarAnimation = function() {
+      template.showToolbars = toolbarAnimation.direction === 'reverse';
+    };
 
     template.handleKeyPress = function(e) {
       switch (e.detail.key) {
@@ -17,6 +35,10 @@
 
         case 'left':
           pageableText.previousPage();
+        break;
+
+        case 'esc':
+          template.toggleToolbars();
         break;
       }
     };
