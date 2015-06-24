55567c89ec4f02d4c5406441dd2e8eb13c8f9390
Jeff Posnick <jeffy@google.com>
2015-06-24 13:47:30 +0100

    More!

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,18 @@
+'use strict';
+
 import del from 'del';
-import {exec} from 'child_process';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
+import sequence from 'run-sequence';
+import {exec} from 'child_process';
 
 const $ = gulpLoadPlugins();
+
 const SRC_DIR = 'src';
 const DEV_DIR = 'dev';
 const DIST_DIR = 'dist';
 const JS_FILES_GLOB = `${SRC_DIR}/js/**/*.js`;
+const SRC_FILES_GLOBS = [`${SRC_DIR}/!(js)/**/*`, `${SRC_DIR}/*.*`];
 
 gulp.task('babel', () => {
   return gulp.src(JS_FILES_GLOB)
@@ -15,22 +20,31 @@ gulp.task('babel', () => {
     .pipe(gulp.dest(`${DEV_DIR}/js`));
 });
 
-gulp.task('bower', () => {
-  return $.bower()
-    .pipe(gulp.dest(DEV_DIR));
+gulp.task('bower', callback => {
+  exec('bower install', callback);
+});
+
+gulp.task('build', callback => {
+  sequence(['bower', 'babel', 'copy-src-files'], 'watch', callback);
 });
 
 gulp.task('clean', callback => {
   del([DEV_DIR, DIST_DIR], callback);
 });
 
-gulp.task('watch', () => {
-  gulp.watch('bower.json', ['bower']);
-  gulp.watch(JS_FILES_GLOB, ['babel']);
-});
-
-gulp.task('serve:dev', ['watch'], callback => {
-  exec('node_modules/http-server/bin/http-server dev -c-1', callback);
+gulp.task('copy-src-files', () => {
+  return gulp.src(SRC_FILES_GLOBS)
+    .pipe(gulp.dest(DEV_DIR));
 });
 
 gulp.task('default', ['serve:dev']);
+
+gulp.task('serve:dev', ['build'], callback => {
+  exec('node_modules/http-server/bin/http-server dev -c-1', callback);
+});
+
+gulp.task('watch', () => {
+  gulp.watch('bower.json', ['bower']);
+  gulp.watch(JS_FILES_GLOB, ['babel']);
+  gulp.watch(SRC_FILES_GLOBS, ['copy-src-files']);
+});
