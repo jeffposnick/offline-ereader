3bb603af0274e15f58eb7fb31f1ea384f7f6b846
Jeff Posnick <jeffy@google.com>
2016-12-22 17:07:41 -0500

    Moving closer to a working read

--- src/read.js
+++ src/read.js
@@ -5,9 +5,24 @@ import './styles/read.css';
 import idbOrWorker from './lib/idb-or-worker';
 import markdownToHtmlWorker from './lib/workers/markdown-to-html-worker';
 
+let textElement;
+let totalPages;
+
+const setTranslateOffset = offset => {
+  textElement.style.transform = `translateX(-${offset}px)`;
+};
+
+const updateState = () => {
+  totalPages = Math.ceil(textElement.scrollWidth / (textElement.clientWidth + 40));
+  console.log(totalPages);
+};
+
 (async () => {
   const url = decodeURIComponent(location.search.substring(1));
   const bookHtml = await idbOrWorker(url, markdownToHtmlWorker);
   const container = document.querySelector('main');
-  container.innerHTML = bookHtml;
+  const containerHtml = `<div class="container"><div class="text">${bookHtml}</div></div>`;
+  container.innerHTML = containerHtml;
+  textElement = document.querySelector('.text');
+  updateState();
 })();
