ea1c212ccb8482e3b26f12c21f50124f421394e5
Jeff Posnick <jeffy@google.com>
2016-12-23 00:58:42 -0500

    Some click handlers.

--- src/read.js
+++ src/read.js
@@ -5,16 +5,47 @@ import './styles/read.css';
 import idbOrWorker from './lib/idb-or-worker';
 import markdownToHtmlWorker from './lib/workers/markdown-to-html-worker';
 
+const columnGap = 40;
+
+let currentPage;
 let textElement;
 let totalPages;
 
-const setTranslateOffset = offset => {
-  textElement.style.transform = `translateX(-${offset}px)`;
+const nextPage = () => {
+  if (currentPage < totalPages) {
+    currentPage += 1;
+  }
+  flipToCurrentPage();
+};
+
+const previousPage = () => {
+  if (currentPage > 1) {
+    currentPage -= 1;
+  }
+  flipToCurrentPage();
+};
+
+const clickHandler = event => {
+  if (event.clientX >= (window.innerWidth / 2)) {
+    nextPage();
+  } else {
+    previousPage();
+  }
+};
+
+const setupListeners = () => {
+  textElement = document.querySelector('.text');
+  textElement.addEventListener('click', clickHandler);
 };
 
 const updateState = () => {
-  totalPages = Math.ceil(textElement.scrollWidth / (textElement.clientWidth + 40));
-  console.log(totalPages);
+  currentPage = 1;
+  totalPages = Math.ceil(textElement.scrollWidth / (textElement.clientWidth + columnGap));
+};
+
+const flipToCurrentPage = () => {
+  const translateOffset = (currentPage - 1) * (textElement.clientWidth + columnGap);
+  textElement.style.transform = `translateX(-${translateOffset}px)`;
 };
 
 (async () => {
@@ -23,6 +54,7 @@ const updateState = () => {
   const container = document.querySelector('main');
   const containerHtml = `<div class="container"><div class="text">${bookHtml}</div></div>`;
   container.innerHTML = containerHtml;
-  textElement = document.querySelector('.text');
+
+  setupListeners();
   updateState();
 })();
