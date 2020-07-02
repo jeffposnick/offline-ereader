a9b023c624fb386c9a7f5cc1048faff1e6db66c4
Jeff Posnick <jeffy@google.com>
2020-07-02 14:02:39 -0400

    Some changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,48 @@
 <script>
-	const message = 'Learn Svelte';
+	let text = '';
+	for (let i = 0; i <= 1000; i++) {
+		text += `<p>Testing ${i}</p>`;
+	}
+	
+	function handleClick(event) {
+		const main = document.querySelector('main');
+		const width = main.clientWidth;
+
+		if (event.screenX >= width / 2) {
+			console.log('f');
+			document.querySelector('main').scroll({
+				left: main.scrollLeft + main.clientWidth + 4,
+				behavior: 'smooth',
+			});
+		} else {
+			console.log('b');
+			document.querySelector('main').scroll({
+				left: main.scrollLeft - main.clientWidth - 4,
+				behavior: 'smooth',
+			});
+		}
+	}
 </script>
 
 <style>
 	:global(body) {
+		font-family: serif;
+		font-size: 400%;
 		margin: 0;
-		font-family: Arial, Helvetica, sans-serif;
 	}
-	.App {
-		text-align: center;
-	}
-	.App-header {
-		background-color: #F9F6F6;
-		color: #333;
-		min-height: 100vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		font-size: calc(10px + 2vmin);
-	}
-	.App-link {
-		color: #ff3e00;
-	}
-	.App-logo {
-		height: 40vmin;
-		pointer-events: none;
-		margin-bottom: 1.0rem;
-		animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-	}
-	@keyframes App-logo-spin {
-		from {
-			transform: scale(1);
-		}
-		to {
-			transform: scale(1.06);
-		}
+
+	main {
+		column-count: 2;
+		column-gap: 4px;
+		cursor: pointer;
+		max-height: 70vh;
+		overflow-x: hidden;
+		user-select: none;
 	}
 </style>
 
-<div class="App">
-	<header class="App-header">
-		<img src="/logo.svg" class="App-logo" alt="logo" />
-		<p>
-			Edit <code>src/App.svelte</code> and save to reload.
-		</p>
-		<a
-			class="App-link"
-			href="https://svelte.dev"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			{message}
-		</a>
-	</header>
+<div>
+	<main on:click={handleClick}>
+		{@html text}
+	</main>
 </div>
